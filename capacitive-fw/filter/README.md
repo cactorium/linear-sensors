85b516185521dbe2803a242170d04ed12b22cd72
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-07-15 10:52:43 -0400

    Move a lot of files around and add README

---
+++ capacitive-fw/filter/README.md
@@ -0,0 +1,1 @@
+Testing out the FIR filters to use on the STM32; there's a lot of 60 Hz noise and harmonics in the signal, so we need to filter them out before using the periodogram to calculate phase.
---
+++ capacitive-fw/filter/fir.cc
@@ -0,0 +1,101 @@
+#include <array>
+#include <vector>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#define FIR_HP_BUFFER_SZ 100
+#define FIR_LP_BUFFER_SZ1 64
+#define FIR_LP_BUFFER_SZ2 32
+
+// hp, 100 window
+int32_t feed_fir1(uint16_t i) {
+  static uint16_t buf[FIR_HP_BUFFER_SZ] = {0};
+  static uint8_t half_ptr = FIR_HP_BUFFER_SZ/2;
+  static uint8_t cur_ptr = 0;
+  static uint32_t sum = 0;
+
+  sum += i;
+  sum -= buf[cur_ptr];
+  buf[cur_ptr] = i;
+
+  ++cur_ptr;
+  if (cur_ptr >= FIR_HP_BUFFER_SZ) {
+    cur_ptr = 0;
+  }
+  ++half_ptr;
+  if (half_ptr >= FIR_HP_BUFFER_SZ) {
+    half_ptr = 0;
+  }
+  
+  return FIR_HP_BUFFER_SZ*buf[half_ptr] - sum;
+}
+
+// lp, 64 window
+int32_t feed_fir2(int32_t i) {
+  static int32_t buf[FIR_LP_BUFFER_SZ1] = {0};
+  static uint8_t cur_ptr = 0;
+  static int32_t sum = 0;
+
+  sum += i;
+  sum -= buf[cur_ptr];
+  buf[cur_ptr] = i;
+
+  ++cur_ptr;
+  if (cur_ptr >= FIR_LP_BUFFER_SZ1) {
+    cur_ptr = 0;
+  }
+  
+  return sum;
+}
+
+// lp, 32 window
+uint32_t feed_fir3(uint16_t) {
+  static int32_t buf[FIR_LP_BUFFER_SZ2] = {0};
+  static uint8_t cur_ptr = 0;
+  static int32_t sum = 0;
+
+  sum += i;
+  sum -= buf[cur_ptr];
+  buf[cur_ptr] = i;
+
+  ++cur_ptr;
+  if (cur_ptr >= FIR_LP_BUFFER_SZ2) {
+    cur_ptr = 0;
+  }
+  
+  return sum;
+
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cerr << "USAGE: " << argv[0] << " INPUT" << std::endl;
+    return 0;
+  }
+  std::ifstream f(argv[1]);
+  std::string s;
+
+  std::getline(f, s);
+
+  std::vector<uint16_t> data;
+  int previous = 0;
+  int pos = s.find(',');
+  while (pos != std::string::npos) {
+    auto numstr = s.substr(previous, pos-previous);
+    auto val = std::stoul(numstr, nullptr, 16);
+    // std::cout << previous << " " << pos << " " << numstr << " " << val << std::endl;
+    data.push_back(val);
+    previous = pos + 1;
+    pos = s.find(',', previous);
+  }
+
+  int count = 0;
+  for (auto i: data) {
+    std::cout << count << ":" << i << ",";
+    ++count;
+  }
+  std::cout << std::endl;
+  return 0;
+}
