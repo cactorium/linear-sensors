d10aef6b88b2b19b0e7cf78b44bec7f11afac904
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-07-20 21:42:41 -0400

    Relayout capacitive scale and regenerate all Gerber files

--- capacitive/gen_pads.sh
+++ capacitive/gen_pads.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCALE_LENGTH=200.00
+SCALE_LENGTH=320.00
 
 python padgen/gen_tx_pads.py > capacitive.pretty/TX_PADS.kicad_mod
 python padgen/gen_rx_pad.py > capacitive.pretty/RX_PAD.kicad_mod
